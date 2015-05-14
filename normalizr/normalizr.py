53dbed29b25d876b86da8fb200361dab17df8463
David Moreno García <david.mogar@gmail.com>
2015-05-14 16:18:45 +0200

    Remove debug code

--- normalizr/normalizr.py
+++ normalizr/normalizr.py
@@ -98,7 +98,6 @@ class Normalizr:
         Returns:
             The text without accent marks.
         """
-
         return unicodedata.normalize('NFKC', ''.join(c for c in unicodedata.normalize('NFKD', text)
                        if unicodedata.category(c) != 'Mn' or c in excluded))
 
