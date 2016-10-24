5fa65e87fc4f23f4b10e1f3b3605232f66a2bca2
Ben Feinstein <ben.feinstein@mail.huji.ac.il>
2016-10-24 14:44:52 +0300

    Add tests
    - Start testing the `normalizr` module.
    - Start testing the `regex` module.
    
    Needs PR #9 to pass all tests on python 3.5.

---
+++ tests/test_normalizr.py
@@ -0,0 +1,238 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+
+import sys
+import unittest
+
+from normalizr import normalizr
+
+IS_PY3 = sys.version_info[0] == 3
+
+if not IS_PY3:
+    from codecs import open
+
+
+def read_file(path, encoding='utf-8'):
+    """Read text from file."""
+    with open(path, encoding=encoding) as f:
+        return f.read()
+
+
+class TestNormalizr(unittest.TestCase):
+    def setUp(self):
+        self._normalizr = normalizr.Normalizr()
+
+    def test_remove_accent_marks(self):
+        test_cases = [
+            {
+                'name': 'empty string',
+                'before': "",
+                'after': ""
+            },
+            {
+                'name': 'wikipedia italian',
+                'before': "Wikipedia è un'enciclopedia online, collaborativa e culturalmente libera.",
+                'after': "Wikipedia e un'enciclopedia online, collaborativa e culturalmente libera."
+            },
+        ]
+
+        for test_case in test_cases:
+            name, before, after = test_case['name'], test_case['before'], test_case['after']
+            self.assertEqual(self._normalizr.remove_accent_marks(before), after)
+
+    def test_remove_extra_whitespaces(self):
+        test_cases = [
+            {
+                'name': 'empty string',
+                'before': "",
+                'after': ""
+            },
+            {
+                'name': 'no extra whitespaces',
+                'before': "Who let the dog out?",
+                'after': "Who let the dog out?"
+            },
+            {
+                'name': 'new line',
+                'before': "Who let\nthe dog out?",
+                'after': "Who let the dog out?"
+            },
+            {
+                'name': 'double space',
+                'before': "Who let  the dog out?",
+                'after': "Who let the dog out?"
+            },
+            {
+                'name': 'strip',
+                'before': " Who let  the dog out? ",
+                'after': "Who let the dog out?"
+            },
+            {
+                'name': 'complex',
+                'before': "  Who\tlet \n the dog out?\n\n\n\t",
+                'after': "Who let the dog out?"
+            },
+        ]
+
+        for test_case in test_cases:
+            name, before, after = test_case['name'], test_case['before'], test_case['after']
+            self.assertEqual(self._normalizr.remove_extra_whitespaces(before), after)
+
+    def test_replace_emojis(self):
+        test_cases = [
+            {
+                'name': 'empty string',
+                'before': "",
+                'after': ""
+            },
+            {
+                'name': 'no emoji',
+                'before': "Grinning Face",
+                'after': "Grinning Face"
+            },
+            {
+                'name': 'smiley default replacement',
+                'before': "😀 Grinning Face",
+                'after': " Grinning Face"
+            },
+            {
+                'name': 'smiley custom replacement',
+                'before': "😀 Grinning Face",
+                'kwargs': {'replacement': ':-)'},
+                'after': ":-) Grinning Face"
+            },
+        ]
+
+        for test_case in test_cases:
+            name, before, kwargs, after = \
+                test_case['name'], test_case['before'], test_case.get('kwargs', dict()), test_case['after']
+            self.assertEqual(self._normalizr.replace_emojis(text=before, **kwargs), after)
+
+    def test_replace_hyphens(self):
+        test_cases = [
+            {
+                'name': 'empty string',
+                'before': "",
+                'after': ""
+            },
+            {
+                'name': 'no hyphens',
+                'before': "New York",
+                'after': "New York"
+            },
+            {
+                'name': 'hyphen default replacement',
+                'before': "New-York",
+                'after': "New York"
+            },
+            {
+                'name': 'hyphen custom replacement',
+                'before': "New-York",
+                'kwargs': {'replacement': '_'},
+                'after': "New_York"
+            },
+        ]
+
+        for test_case in test_cases:
+            name, before, kwargs, after = \
+                test_case['name'], test_case['before'], test_case.get('kwargs', dict()), test_case['after']
+            self.assertEqual(self._normalizr.replace_hyphens(text=before, **kwargs), after)
+
+    def test_replace_punctuation(self):
+        test_cases = [
+            {
+                'name': 'empty string',
+                'before': "",
+                'after': ""
+            },
+            {
+                'name': 'no punctuation',
+                'before': "How let the dog out",
+                'after': "How let the dog out"
+            },
+            {
+                'name': 'question mark default replacement',
+                'before': "How let the dog out?",
+                'after': "How let the dog out"
+            },
+            {
+                'name': 'question mark custom replacement',
+                'before': "How let the dog out?",
+                'kwargs': {'replacement': ' '},
+                'after': "How let the dog out "
+            },
+            {
+                'name': 'simple sentence default replacement',
+                'before': "With normalizr you can replace symbols, punctuation, remove stop words and much more.",
+                'after': "With normalizr you can replace symbols punctuation remove stop words and much more"
+            },
+            {
+                'name': 'simple sentence custom replacement',
+                'before': "With normalizr you can replace symbols, punctuation, remove stop words and much more.",
+                'kwargs': {'replacement': '_'},
+                'after': "With normalizr you can replace symbols_ punctuation_ remove stop words and much more_"
+            },
+            {
+                'name': 'simple sentence dot excluded',
+                'before': "With normalizr you can replace symbols, punctuation, remove stop words and much more.",
+                'kwargs': {'excluded': {'.'}},
+                'after': "With normalizr you can replace symbols punctuation remove stop words and much more."
+            },
+        ]
+
+        for test_case in test_cases:
+            name, before, kwargs, after = \
+                test_case['name'], test_case['before'], test_case.get('kwargs', dict()), test_case['after']
+            self.assertEqual(self._normalizr.replace_punctuation(text=before, **kwargs), after)
+
+    def test_replace_characters(self):
+        test_cases = [
+            {
+                'name': 'empty string',
+                'before': "",
+                'characters': "",
+                'after': ""
+            },
+            {
+                'name': 'no characters',
+                'before': "How let the dog out?",
+                'characters': "",
+                'after': "How let the dog out?"
+            },
+            {
+                'name': 'vowels',
+                'before': "How let the dog out?",
+                'characters': "aeiouAEIOU",
+                'after': "Hw lt th dg t?"
+            },
+            {
+                'name': 'vowels to _',
+                'before': "How let the dog out?",
+                'characters': "aeiouAEIOU",
+                'kwargs': {'replacement': "_"},
+                'after': "H_w l_t th_ d_g __t?"
+            },
+            {
+                'name': 'question mark',
+                'before': "How let the dog out?",
+                'characters': "?",
+                'after': "How let the dog out?"
+            },
+            {
+                'name': 'question mark to exclamation mark',
+                'before': "How let the dog out?",
+                'characters': "?",
+                'kwargs': {'replacement': "!"},
+                'after': "How let the dog out!"
+            },
+        ]
+
+        for test_case in test_cases:
+            name, before, characters, kwargs, after = \
+                test_case['name'], test_case['before'], test_case['characters'], test_case.get('kwargs', dict()), \
+                test_case['after']
+            self.assertEqual(self._normalizr.replace_characters(text=before, characters=characters, **kwargs), after)
+
+
+if __name__ == '__main__':
+    unittest.main()
