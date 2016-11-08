d9e160df4e16e530af67bddad8453c23c31d15f6
Ben Feinstein <ben.feinstein@mail.huji.ac.il>
2016-11-08 15:02:47 +0200

    Optimize emojis normalization Compile the emojis regex once, not every time the function is called

--- normalizr/regex.py
+++ normalizr/regex.py
@@ -14,3 +14,8 @@ URL_REGEX = re.compile(
 Regular expression to match email addresses as seen on http://www.wellho.net/resources/ex.php4?item=y115/relib.py
 """
 EMAIL_REGEX = re.compile(r"(?:^|\s)[-a-z0-9_.]+@(?:[-a-z0-9]+\.)+[a-z]{2,6}(?:\s|$)", re.IGNORECASE)
+
+try:
+    EMOJI_REGEX = re.compile(u'([\U00002600-\U000027BF])|([\U0001f300-\U0001f64F])|([\U0001f680-\U0001f6FF])')
+except re.error:
+    EMOJI_REGEX = re.compile(u'([\u2600-\u27BF])|([\uD83C][\uDF00-\uDFFF])|([\uD83D][\uDC00-\uDE4F])|([\uD83D][\uDE80-\uDEFF])')
