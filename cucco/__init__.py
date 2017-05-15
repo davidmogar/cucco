81227ac87859e0e6bcb1e488cbb0ccf03282abb4
David Moreno García <david.mogar@cern.ch>
2017-05-15 12:58:20 +0200

    Prepare 1.1.0 release

--- cucco/__init__.py
+++ cucco/__init__.py
@@ -4,6 +4,6 @@ This module allows to normalize texts applying different normalizations.
 
 from __future__ import absolute_import
 
-__version__ = '1.0.0'
+__version__ = '1.1.0'
 
 from cucco.cucco import Cucco
