ceab84b1f1d392335dbd7c7f0899dc28d4b7e0f7
David Moreno García <david.mogar@gmail.com>
2017-05-16 23:57:43 +0200

    Add context to commands

--- cucco/cli.py
+++ cucco/cli.py
@@ -9,7 +9,8 @@ from cucco.cucco import Cucco
 
 @click.command()
 @click.argument('path')
-def batch(path):
+@click.pass_context
+def batch(ctx, path):
     """
     Normalize files in path.
 
@@ -23,7 +24,8 @@ def batch(path):
 
 @click.command()
 @click.argument('text', required=False)
-def normalize(text):
+@click.pass_context
+def normalize(ctx, text):
     """
     Normalize text or piped input.
 
@@ -48,7 +50,8 @@ def normalize(text):
 @click.option('--verbose', is_flag=True,
               help='Increase output verbosity.')
 @click.version_option()
-def cli(config, verbose):
+@click.pass_context
+def cli(ctx, config, verbose):
     """
     Cucco allows to apply normalizations to a given text or file.
     This normalizations include, among others, removal of accent
@@ -59,7 +62,8 @@ def cli(config, verbose):
     project website at https://cucco.io.
     """
 
-    pass
+    ctx.obj['config'] = config # todo: Assing actual config content
+    ctx.obj['VERBOSE'] = verbose
 
 cli.add_command(batch)
 cli.add_command(normalize)
