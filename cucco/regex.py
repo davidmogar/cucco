51c53dbf6fcfb0242df8f145e7b07f52ac6c6516
David Moreno García <david.mogar@gmail.com>
2017-06-01 22:24:32 +0200

    Set back regex alternative
    Turns out that although Travis was happy with the first regex,
    this doesn't works in my Macbook and possibily in other systems.

--- cucco/regex.py
+++ cucco/regex.py
@@ -15,4 +15,7 @@ Regular expression to match email addresses as seen on http://www.wellho.net/res
 """
 EMAIL_REGEX = re.compile(r"[-a-z0-9_.]+@(?:[-a-z0-9]+\.)+[a-z]{2,6}", re.IGNORECASE)
 
-EMOJI_REGEX = re.compile(r'([\U00002600-\U000027BF\U0001F300-\U0001F64F\U0001F680-\U0001F6FF])')
+try:
+    EMOJI_REGEX = re.compile(u'([\U00002600-\U000027BF])|([\U0001f300-\U0001f64F])|([\U0001f680-\U0001f6FF])')
+except re.error:
+    EMOJI_REGEX = re.compile(u'([\u2600-\u27BF])|([\uD83C][\uDF00-\uDFFF])|([\uD83D][\uDC00-\uDE4F])|([\uD83D][\uDE80-\uDEFF])')
