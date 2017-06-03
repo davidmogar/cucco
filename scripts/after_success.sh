c1d69d71c99ee42d390e7f959f5ceb003851cad7
David Moreno García <david.mogar@gmail.com>
2017-06-03 19:32:47 +0200

    Add codeclimate

---
+++ scripts/after_success.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]
+then
+    CODECLIMATE_REPO_TOKEN=8eca2b2af30c60b05b6bc5ea848ce2e3d52da97ae0c1f923b612af8461bb2fc3 codeclimate-test-reporter
+    codecov
+fi
