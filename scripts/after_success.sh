3afd6902b618a12348870c05e614df243a171d7a
David Moreno García <david.mogar@gmail.com>
2017-06-03 19:54:25 +0200

    Add echo messages

--- scripts/after_success.sh
+++ scripts/after_success.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]
-then
+if [[ "${TRAVIS_PULL_REQUEST}" == "false" ]]; then
+    echo 'Submitting report'
     CODECLIMATE_REPO_TOKEN=8eca2b2af30c60b05b6bc5ea848ce2e3d52da97ae0c1f923b612af8461bb2fc3 codeclimate-test-reporter
     codecov
+else
+    echo 'This is a pull request. Report will not be submitted.'
 fi
